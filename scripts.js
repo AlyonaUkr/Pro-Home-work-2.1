78ebde5abbe84ae790bdfb1ff366028d0e2fef33
AlyonaUkr <mochulska1994@gmail.com>
2022-12-01 13:00:08 +0200

    ready

---
+++ scripts.js
@@ -0,0 +1,17 @@
+const firstNumber = prompt('Write first number, please');
+const secondNumber = prompt('Write second number, please');
+const plusValue = +firstNumber + +secondNumber;
+
+alert(plusValue);
+
+const minusValue = +firstNumber - +secondNumber;
+
+alert(minusValue);
+
+const multiValue = +firstNumber * +secondNumber;
+
+alert(multiValue);
+
+const divideValue = +firstNumber / +secondNumber;
+
+alert(divideValue);
